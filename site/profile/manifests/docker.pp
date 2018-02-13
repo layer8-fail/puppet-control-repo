fbcf1e65b8f852d01c04f003307de424835421c4
Mattias Giese <giese@b1-systems.de>
2018-02-13 21:29:50 +1300

    profile::docker: Well, some port access would be nice

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -1,4 +1,27 @@
-class profile::docker() {
+class profile::docker(
+  $manage_firewalld = true,
+  $docker_ports  = [
+    {
+      'port'     => '80',
+      'protocol' => 'tcp',
+    },
+    {
+      'port'     => '443',
+      'protocol' => 'tcp',
+    },
+  ]
+) {
   class { '::docker': } ->
   class { '::traefik': }
+
+  if $manage_firewalld {
+    $docker_ports.each |Hash $item| {
+      firewalld_port { "Docker: Allow access from remote, Port ${item['port']}/${item['protocol']}":
+        ensure   => present,
+        zone     => 'public',
+        port     => "${item['port']}",
+        protocol => "${item['protocol']}",
+      }
+    }
+  }
 }
