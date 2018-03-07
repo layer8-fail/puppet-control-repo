e3243b4ef38892672f7b092560e8d8c23198d492
Mattias Giese <mattiasgiese@posteo.net>
2018-03-07 21:03:29 +1300

    pdk convert profile module, new class profile::pkgbuilder

---
+++ site/profile/manifests/pkgbuilder.pp
@@ -0,0 +1,10 @@
+# profile::pkgbuilder
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include profile::pkgbuilder
+class profile::pkgbuilder {
+}
