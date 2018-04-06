8ef33f75bafa665d16c28271f5f3d2306230fd43
Mattias Giese <mattiasgiese@posteo.net>
2018-04-06 22:39:28 +1200

    profile::puppetrun: Stdlib::Absolutepath not working in a template? Am i too tired already?

--- site/profile/manifests/puppetrun.pp
+++ site/profile/manifests/puppetrun.pp
@@ -1,11 +1,11 @@
 class profile::puppetrun(
-  $path          = '/usr/local/bin/puppetrun',
-  $template      = "${module_name}/puppetrun.sh.epp",
-  $environment   = 'production',
-  $codedir       = '/etc/puppetlabs/code',
-  $puppet_binary = '/opt/puppetlabs/bin/puppet',
-  $manifest      = 'site.pp',
-  $flags         = '-v',
+  Stdlib::Absolutepath $path          = '/usr/local/bin/puppetrun',
+  String $template                    = "${module_name}/puppetrun.sh.epp",
+  String $environment                 = 'production',
+  Stdlib::Absolutepath $codedir       = '/etc/puppetlabs/code',
+  Stdlib::Absolutepath $puppet_binary = '/opt/puppetlabs/bin/puppet',
+  String $manifest                    = 'site.pp',
+  String $flags                       = '-v',
 ) {
   file{$path:
     ensure  => file,
