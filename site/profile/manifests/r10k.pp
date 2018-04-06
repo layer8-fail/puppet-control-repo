24ba298580587244e6fabedb9a352b163929ed44
Mattias Giese <mattiasgiese@posteo.net>
2018-04-06 23:54:02 +1200

    profile::r10k: remove bogus certificate directives from r10k::webhook::config class

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -13,8 +13,6 @@ class profile::r10k(
   } ->
   class { 'r10k::webhook::config':
     use_mcollective  => false,
-    public_key_path  => '/etc/mcollective/server_public.pem',  # Mandatory even when use_mcollective is false
-    private_key_path => '/etc/mcollective/server_private.pem', # Mandatory even when use_mcollective is false
   }
   class { 'r10k::webhook':
     user    => 'root',
