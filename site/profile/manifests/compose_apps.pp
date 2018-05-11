77bfd9d66a3ba4600146e1d937ec1f2ab6ab2c93
Mattias Giese <mattiasgiese@posteo.net>
2018-05-11 18:07:13 +1200

    p::compose_apps: initial commit
    Add a super ghetto way to quickly spawn docker-compose based
    apps. This profile will check out a git repository and try
    to start every app defined in the array 'apps'

---
+++ site/profile/manifests/compose_apps.pp
@@ -0,0 +1,27 @@
+class profile::compose_apps (
+  Boolean $manage_vcs_repo = true,
+  String $vcs_repo = 'https://github.com/layer8-fail/docker-stuff.git',
+  String $dir = '/opt/docker-stuff'
+  String $compose_dir = 'compose',
+  String $vcs_branch = 'master',
+  Array $apps = [
+    'archlinux',
+    'aur_repo',
+    'gitea',
+    'netdata',
+  ]
+) {
+  vcsrepo { $dir:
+  ensure   => present,
+  provider => git,
+  source   => $vcs_repo,
+  revision => $vcs_branch,
+  }
+
+  $apps.each |String $app| {
+    docker_compose{ "${dir}/${compose_dir}/${app}/docker-compose.yaml":
+      ensure    => present,
+      subscribe => Vcsrepo[$dir],
+    }
+  }
+}
--- site/role/manifests/everything.pp
+++ site/role/manifests/everything.pp
@@ -2,4 +2,5 @@ class role::everything() {
   class { '::role::base': } ->
   class { '::profile::docker': } ->
   class { '::profile::syncthing': }
+  class { '::profile::compose_apps': }
 }
