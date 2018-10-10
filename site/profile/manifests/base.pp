f848fd2b2010f1da1c73291c0396241bff827dda
Mattias Giese <mattiasgiese@posteo.net>
2018-10-10 19:24:47 +0000

    Add basic sudoers configuration

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,9 @@ class profile::base {
   contain ::ntp
   contain ::ssh
   include ::firewalld
+  class { 'sudo':
+    purge               => false,
+    config_file_replace => false,
+  }
+  include sudo::configs
 }
