8d9dec3353a8b1552ab7b5ae4f1bb3790d17ed2f
Mattias Giese <mattiasgiese@posteo.net>
2019-04-23 18:53:42 -0500

    glpi: initial skeleton

---
+++ site/role/manifests/glpi.pp
@@ -0,0 +1,8 @@
+# Role which activates a fullblown instance of GLPI on a node
+#
+# @summary Role which activates a fullblown instance of GLPI on a node
+#
+# @example
+#   include role::glpi
+class role::glpi {
+}
