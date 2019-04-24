59a70271759d2e46659d5b4c34a64cefaf268dcc
Mattias Giese <mattiasgiese@posteo.net>
2019-04-24 11:34:05 -0500

    profile::glpi_standalone: add nginx location resource

--- site/profile/manifests/glpi_standalone.pp
+++ site/profile/manifests/glpi_standalone.pp
@@ -9,7 +9,7 @@ class profile::glpi_standalone (
   Boolean $manage_repos    = true,
   String $upstream_url     = 'glpi.lab.fail',
   Integer $port            = 80,
-  String $php_fpm_url      = 'http://localhost:9000',
+  String $php_fpm_url      = 'localhost:9000',
   String $www_root = '/var/www/glpi/current',
 ){
   if ! $facts['os']['family'] == 'RedHat' {
@@ -27,18 +27,30 @@ class profile::glpi_standalone (
 
   nginx::resource::server { $upstream_url:
     listen_port => $port,
-    proxy       => $php_fpm_url,
     www_root    => $www_root,
-    index_files => [ 'index.php' ],
   }
+  nginx::resource::location { 'glpi_root':
+    ensure          => present,
+    server          => $upstream_url,
+    www_root        => $www_root,
+    location        => '~ \.php$',
+    index_files     => ['index.php'],
+    proxy           => undef,
+    fastcgi         => $php_fpm_url,
+    fastcgi_script  => undef,
+    location_cfg_append => {
+      fastcgi_connect_timeout => '3m',
+      fastcgi_read_timeout    => '3m',
+      fastcgi_send_timeout    => '3m'
+    }
 
-  if $manage_firewall {
-    ['http','https'].each |$svc| {
-      firewalld_service {"Allow access to $svc":
-        ensure  => present,
-        service => $svc,
-        zone    => 'public',
+    if $manage_firewall {
+      ['http','https'].each |$svc| {
+        firewalld_service {"Allow access to $svc":
+          ensure  => present,
+          service => $svc,
+          zone    => 'public',
+        }
       }
     }
   }
-}
