dab27450b0cefcc95baf09d5ff146e466260fc49
Mattias Giese <mattiasgiese@posteo.net>
2019-04-27 20:17:56 -0500

    profile::icinga_master: fix order of icinga2 usage, again.

--- site/profile/manifests/icinga_master.pp
+++ site/profile/manifests/icinga_master.pp
@@ -12,7 +12,7 @@ class profile::icinga_master {
   }
   class{'icinga2':
     manage_repo => true,
-  } ->
+  }
   class{ '::icinga2::feature::idopgsql':
     user          => 'icinga2',
     password      => 'supersecret',
