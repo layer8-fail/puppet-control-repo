f80e714b6375eb11c0822c3253be6ab70deb1c96
Mattias Giese <mattiasgiese@posteo.net>
2019-04-27 20:14:50 -0500

    profile::icinga_master: fix order of icinga2 usage

--- site/profile/manifests/icinga_master.pp
+++ site/profile/manifests/icinga_master.pp
@@ -7,18 +7,17 @@
 class profile::icinga_master {
   include ::postgresql::server
   postgresql::server::db { 'icinga2':
-  user     => 'icinga2',
-  password => postgresql_password('icinga2', 'supersecret'),
-}
-
+    user     => 'icinga2',
+    password => postgresql_password('icinga2', 'supersecret'),
+  }
+  class{'icinga2':
+    manage_repo => true,
+  } ->
   class{ '::icinga2::feature::idopgsql':
     user          => 'icinga2',
     password      => 'supersecret',
     database      => 'icinga2',
     import_schema => true,
     require       => Postgresql::Server::Db['icinga2'],
-  }
-  class{'icinga2':
-    manage_repo => true,
   }
 }
