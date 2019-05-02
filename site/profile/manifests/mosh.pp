001a381a96827822037ae6c1dad59b740c75d6d7
Mattias Giese <mattiasgiese@posteo.net>
2019-05-02 18:21:07 -0500

    introduce simple profile::mosh class

---
+++ site/profile/manifests/mosh.pp
@@ -0,0 +1,30 @@
+# This class install mosh and opens the firewall on the given ports
+#
+# @summary Setup mosh, the mobile shell
+#
+# @example
+#   include profile::mosh
+class profile::mosh (
+  Boolean $manage_firewall     = true,
+  String $package              = 'mosh',
+  Array $fw_ports              = ['60000','60001'],
+  Optional[String] $fw_service = undef,
+){
+  if $manage_firewall {
+    if $fw_ports {
+      $fw_ports.each |$svc| {
+        firewalld_port {"Allow access to port ${svc}":
+          ensure   => present,
+          port     => $svc,
+          protocol => 'udp',
+        }
+      }
+    }
+    if $fw_service {
+      firewalld_service {"Allow access to service ${fw_service}":
+        ensure  => present,
+        service => $fw_service,
+      }
+    }
+  }
+}
