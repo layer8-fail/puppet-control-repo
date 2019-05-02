00f645af9f71b83835f775bb164333dd9d0895dc
Mattias Giese <mattiasgiese@posteo.net>
2019-05-02 17:33:54 -0500

    profile::base: install some basic extra packages

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,6 @@
-class profile::base {
+class profile::base (
+  $package_list = []
+){
   if $::facts['os']['family'] == 'Suse' and versioncmp($::facts['os']['release']['major'],'15') >= 0 {
     contain ::chrony
   }
@@ -11,4 +13,6 @@ class profile::base {
     purge               => false,
     config_file_replace => false,
   }
+  # Install some baseline packages
+  ensure_packages($package_list)
 }
