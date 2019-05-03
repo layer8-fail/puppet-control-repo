b1e42894421dda49cb9becd16db6cceca28a65cb
Mattias Giese <mattiasgiese@posteo.net>
2019-05-03 17:09:02 -0500

    Introduce profile::icinga2_director

---
+++ site/profile/manifests/icinga2_director.pp
@@ -0,0 +1,42 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include profile::icinga2_director
+class profile::icinga2_director (
+  Boolean $manage_database  = true,
+  String $db_host           = $db_host,
+  Enum[pgsql] $db_engine    = 'pgsql',
+  String $db_user           = 'icinga2_director',
+  String $db_password       = 'icinga2_director',
+  Integer[1,65535] $db_port = 5432,
+  String $db_name           = 'icinga2_director',
+  String $git_revision      = 'v1.6.2',
+  String $endpoint          = 'monitor.lab.fail',
+  String $api_user          = 'director',
+  String $api_password      = $::profile::icinga_master::api_users['director']['password']
+){
+  if $manage_database {
+    if $db_engine == 'pgsql' {
+      include ::postgresql::server
+      postgresql::server::db { $db_name:
+        user     => $db_user,
+        password => postgresql_password($db_user, $db_password),
+      }
+      Postgresql::Server::Db[$db_name] -> Class['::icingaweb2::module::director']
+    }
+  }
+  class {'icingaweb2::module::director':
+    git_revision  => $git_revision,
+    db_host       => $db_host,
+    db_name       => $db_name,
+    db_username   => $db_user,
+    db_password   => $db_password,
+    import_schema => true,
+    kickstart     => true,
+    endpoint      => $endpoint,
+    api_username  => 'root',
+    api_password  => 'icinga',
+  }
+}
