01cd69b57a3bcd88895321735bf998cfc243a84e
Mattias Giese <mattiasgiese@posteo.net>
2019-05-02 19:50:18 -0500

    profile::icinga_master: more config options

--- site/profile/manifests/icinga_master.pp
+++ site/profile/manifests/icinga_master.pp
@@ -1,23 +1,43 @@
-# A description of what this class does
+# This profile sets up a bare-bone icinga2 master
+# and the corresponding database if desired
 #
-# @summary A short summary of the purpose of this class
+# @summary set up a icinga 2 master
 #
 # @example
 #   include profile::icinga_master
-class profile::icinga_master {
-  include ::postgresql::server
-  postgresql::server::db { 'icinga2':
-    user     => 'icinga2',
-    password => postgresql_password('icinga2', 'supersecret'),
-  }
+class profile::icinga_master (
+  Boolean $manage_database = true,
+  Boolean $manage_ca       = true,
+  Enum[pgsql] $db_engine   = 'pgsql',
+  String $db_host          = 'localhost',
+  String $db_user          = 'icinga2',
+  String $db_password      = 'icinga2',
+  String $db_name          = 'icinga2',
+){
   class{'icinga2':
     manage_repo => true,
   }
+  if $manage_ca {
+    class { '::icinga2::pki::ca': }
+  }
   class{ '::icinga2::feature::idopgsql':
-    user          => 'icinga2',
-    password      => 'supersecret',
-    database      => 'icinga2',
+    user          => $db_user,
+    password      => $db_password,
+    database      => $db_name,
     import_schema => true,
-    require       => Postgresql::Server::Db['icinga2'],
+  }
+  class { '::icinga2::feature::api':
+    accept_commands => true,
+    accept_config   => true,
+  }
+  if $manage_database {
+    if $db_engine == 'pgsql' {
+      include ::postgresql::server
+      postgresql::server::db { $db_name:
+        user     => $db_user,
+        password => postgresql_password($db_user, $db_password),
+      }
+      Postgresql::Server::Db[$db_name] -> Class['::icinga2::feature::idopgsql']
+    }
   }
 }
